e9067747fce8d6925e04766a94a796a485cc41da
radicalroy <roy@thexcompany.com>
2025-01-14 15:43:10 -0800

    convert main list view to flatlist. enable delete and toggle complete for items. utilize flatlist prop components

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,83 @@
-import { StyleSheet, TextInput, View } from "react-native";
+import { StyleSheet, TextInput, FlatList, View, Text } from "react-native";
 import { theme } from "../theme";
 import { ShoppingListItem } from "../components/ShoppingListItem";
+import { useState } from "react";
+
+type ShoppingListItemType = {
+  id: string;
+  name: string;
+  completedAtTimestamp?: number;
+};
 
 export default function App() {
+  const [shoppingList, setShoppingList] = useState<ShoppingListItemType[]>([]);
+  const [inputText, setInputText] = useState("");
+
+  const handleSubmit = () => {
+    if (inputText) {
+      const newShoppingList = [
+        ...shoppingList,
+        { id: new Date().toISOString(), name: inputText },
+      ];
+      setShoppingList(newShoppingList);
+      setInputText("");
+    }
+  };
+
+  const handleDelete = (id: string) => {
+    const newShoppingList = shoppingList.filter((item) => item.id !== id);
+    setShoppingList(newShoppingList);
+  };
+
+  const handleToggleComplete = (id: string) => {
+    const newShoppingList = shoppingList.map((item) => {
+      if (item.id === id) {
+        return {
+          ...item,
+          completedAtTimestamp: item.completedAtTimestamp
+            ? undefined
+            : Date.now(),
+        };
+      }
+      return item;
+    });
+    setShoppingList(newShoppingList);
+  };
+
+  // stickyHeaderIndices will keep elements floating above
+  // in this case the text input will overlay during scroll
   return (
-    <View style={styles.container}>
-      <TextInput style={styles.textInput} />
-      <ShoppingListItem name="Coffee" />
-      <ShoppingListItem name="Tea" isCompleted />
-      <ShoppingListItem name="Sugar" />
-    </View>
+    <FlatList
+      data={shoppingList}
+      renderItem={({ item }) => {
+        return (
+          <ShoppingListItem
+            name={item.name}
+            onDelete={() => handleDelete(item.id)}
+            onToggleComplete={() => handleToggleComplete(item.id)}
+            isCompleted={Boolean(item.completedAtTimestamp)}
+          />
+        );
+      }}
+      stickyHeaderIndices={[0]}
+      style={styles.container}
+      contentContainerStyle={styles.contentContainer}
+      ListEmptyComponent={
+        <View style={styles.listEmptyContainer}>
+          <Text>Your shopping list is empty</Text>
+        </View>
+      }
+      ListHeaderComponent={
+        <TextInput
+          style={styles.textInput}
+          placeholder="E.g. Coffee"
+          value={inputText}
+          onChangeText={setInputText}
+          returnKeyType="done"
+          onSubmitEditing={handleSubmit}
+        />
+      }
+    />
   );
 }
 
@@ -17,12 +85,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.colorWhite,
+    paddingTop: 12,
+  },
+  contentContainer: {
+    paddingBottom: 24,
+  },
+  listEmptyContainer: {
     justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 18,
   },
   textInput: {
     borderColor: theme.colorLightGrey,
     borderWidth: 2,
     padding: 12,
     marginHorizontal: 12,
+    marginBottom: 12,
+    fontSize: 18,
+    borderRadius: 50,
+    backgroundColor: theme.colorWhite,
   },
 });
--- components/ShoppingListItem.tsx
+++ components/ShoppingListItem.tsx
@@ -1,13 +1,27 @@
-import { TouchableOpacity, View, Text, StyleSheet, Alert } from "react-native";
+import {
+  TouchableOpacity,
+  View,
+  Text,
+  StyleSheet,
+  Alert,
+  Pressable,
+} from "react-native";
 import { theme } from "../theme";
 import AntDesign from "@expo/vector-icons/AntDesign";
 
 type Props = {
   name: string;
   isCompleted?: boolean;
+  onDelete: () => void;
+  onToggleComplete: () => void;
 };
 
-export function ShoppingListItem({ name, isCompleted }: Props) {
+export function ShoppingListItem({
+  name,
+  isCompleted,
+  onDelete,
+  onToggleComplete,
+}: Props) {
   const handleDelete = () => {
     Alert.alert(
       `Are you sure you want to delete ${name}?`,
@@ -15,9 +29,7 @@ export function ShoppingListItem({ name, isCompleted }: Props) {
       [
         {
           text: "Yes",
-          onPress: () => {
-            console.log("Ok, deleting");
-          },
+          onPress: onDelete,
           style: "destructive",
         },
         { text: "Cancel", style: "cancel" },
@@ -25,11 +37,12 @@ export function ShoppingListItem({ name, isCompleted }: Props) {
     );
   };
   return (
-    <View
+    <Pressable
       style={[
         styles.itemContainer,
         isCompleted ? styles.completedContainer : undefined,
       ]}
+      onPress={onToggleComplete}
     >
       <Text
         style={[
@@ -46,7 +59,7 @@ export function ShoppingListItem({ name, isCompleted }: Props) {
           color={isCompleted ? theme.colorGrey : theme.colorRed}
         />
       </TouchableOpacity>
-    </View>
+    </Pressable>
   );
 }
 

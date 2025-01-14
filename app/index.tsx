d5a8c4d87751939341e338d5023f71bf4cd7d1e7
radicalroy <roy@thexcompany.com>
2025-01-14 14:42:39 -0800

    implement navigation bottom tabs and stack with additional screens

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,11 @@
-import { StyleSheet, View } from "react-native";
-import { theme } from "./theme";
-import { ShoppingListItem } from "./components/ShoppingListItem";
+import { StyleSheet, TextInput, View } from "react-native";
+import { theme } from "../theme";
+import { ShoppingListItem } from "../components/ShoppingListItem";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <TextInput style={styles.textInput} />
       <ShoppingListItem name="Coffee" />
       <ShoppingListItem name="Tea" isCompleted />
       <ShoppingListItem name="Sugar" />
@@ -17,5 +18,11 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: theme.colorWhite,
     justifyContent: "center",
+  },
+  textInput: {
+    borderColor: theme.colorLightGrey,
+    borderWidth: 2,
+    padding: 12,
+    marginHorizontal: 12,
   },
 });
